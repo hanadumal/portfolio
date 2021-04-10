e897c180e90279c6b106a0ed2fe84ac84af90d0d
hanadumal <hanadumal@outlook.com>
2021-04-10 11:34:54 +0800

    Add macd compute.

---
+++ ws_project/macd/conf.py
@@ -0,0 +1,9 @@
+k15_symbols ={'IOTA/USDT', 'BTC/USDT',  'ETH/USDT', 'XRP/USDT', 'EOS/USDT',
+              'LTC/USDT',  'BCH/USDT',  'ETC/USDT', 'XLM/USDT', 'ADA/USDT',
+              'XMR/USDT',  'DASH/USDT', 'ZEC/USDT', 'OMG/USDT', 'BSV/USDT'}
+
+main_symbols={'BTC/USDT', 'ETH/USDT', 'BNB/USDT'}
+all_symbols =set.union(k15_symbols, main_symbols)
+dev_symbols={'BTC/USDT'}
+all_symbols={'BTC/USDT', 'ETH/USDT', 'BNB/USDT'}
+all_symbols={'SAND/USDT'}
